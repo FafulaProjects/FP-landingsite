7df4156c6c455c02b53014e22f45058c8eb60bba
FafulaProjects <adolf@fafulaprojects.com>
2025-11-30 16:35:04 -0500

    Update Navbar.vue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -15,9 +15,13 @@ const menuitems = [
   <Container>
     <header class="flex flex-row flex-wrap items-center justify-between my-4 gap-x-6 gap-y-4">
       <div class="flex items-center justify-between">
-        <a href="/" class="text-2xl font-bold text-zinc-800">
-          <span class="text-green-500">&#10070;</span> Nuxt Starter
-        </a>
+        <a href="/" class="flex items-center">
+  <img 
+    src="/images/LOGO-Blue1050x600.png" 
+    alt="Fafula Projects Logo" 
+    class="h-10 w-auto object-contain"
+  />
+</a>
       </div>
       <nav class="flex w-auto">
         <ul class="flex flex-row gap-3 text-sm">
